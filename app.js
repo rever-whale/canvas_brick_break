f1b37c54b5db11ffe173ca97408dc6a31a1a6b5f
rever-whale <rever.whale@gmail.com>
2019-08-23 21:42:08 +0900

    update collision logic

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
+// state
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-const dx = 2;
-const dy = -2;
+let dx = 2;
+let dy = -2;
+const ballRadius = 10;
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
   ctx.fillStyle = '#0095DD';
   ctx.fill();
   ctx.closePath();
@@ -18,8 +20,18 @@ function draw() {
   // drawing code
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+
+  if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+    dy = -dy;
+  }
+
+  if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+    dx = -dx;
+  }
+
   x += dx;
   y += dy;
 }
 
 setInterval(draw, 10);
+// requestAnimationFrame(draw);
