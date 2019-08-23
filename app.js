7d9156a0c426be8ceb802f8a1dddb7b0183ea2e5
rever-whale <rever.whale@gmail.com>
2019-08-23 21:50:47 +0900

    add moving paddle

--- app.js
+++ app.js
@@ -8,6 +8,43 @@ let dx = 2;
 let dy = -2;
 const ballRadius = 10;
 
+// paddle state
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+// button state
+let rightPressed = false;
+let leftPressed = false;
+
+// event binding
+document.onkeydown = keyDownHandler;
+document.onkeyup = keyUpHandler;
+
+function keyDownHandler(e) {
+  if (e.keyCode === 39) {
+    rightPressed = true;
+  } else if (e.keyCode === 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.keyCode === 39) {
+    rightPressed = false;
+  } else if (e.keyCode === 37) {
+    leftPressed = false;
+  }
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
+}
+
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -20,6 +57,7 @@ function draw() {
   // drawing code
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
 
   if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
     dy = -dy;
@@ -31,6 +69,12 @@ function draw() {
 
   x += dx;
   y += dy;
+
+  if (rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
 }
 
 setInterval(draw, 10);
